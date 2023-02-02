a2e70b454aeff99e09edc5cfa746dfe2717314f2
Joe Burgess <bojurgess@gmail.com>
2023-02-02 00:28:27 +0000

    Add dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { Auth } from './auth.js'
 import { Api } from './api.js'
-import { CurrentlyPlaying } from './endpoints/currentlyPlaying.js'
+import { CurrentlyPlaying } from './endpoints/player/currentlyPlaying.js'
+
+import * as dotenv from 'dotenv'
+dotenv.config()
 
 export class SpotifySDK {
   private auth: Auth;
@@ -31,8 +34,8 @@ export class SpotifySDK {
   }
 }
 
-const accessToken = 'BQDwnszctplKI3C5yK_F3cQ1UBXAKiT0R-hk0OTXqfjlYfeVfr-IcR9KT6F4eMEebhtBV3Q7AF305eGSfOLSls815hR19IFlz9S-e4Pr-EJSHmHKJq64HmCGblsFBbKV4Ey1Yfs1W0_IxKQGeXL73Ma5AvyYhzZSZwlPKP6N1WppbKtN-P8zLrDD05DdPfAo746lJU8-J6dpLw'
-const baseUrl = 'https://api.spotify.com/v1'
+const accessToken = process.env.SPOTIFY_ACCESS_TOKEN
+const baseUrl = process.env.SPOTIFY_BASE_URL
 
 const sdk = new SpotifySDK()
 
